7292fe155ca94e9f743e586ff46ce70f1d6327ab
kiet <lekiett2201@gmail.com>
2026-06-02 21:44:02 +0700

    initial project

---
+++ server/webrtc/message.go
@@ -0,0 +1,7 @@
+type Message struct {
+	Type   string `json:"type"`
+	RoomID string `json:"roomId,omitempty"`
+	From   string `json:"from,omitempty"`
+	To     string `json:"to,omitempty"`
+	Data   any    `json:"data,omitempty"`
+}
